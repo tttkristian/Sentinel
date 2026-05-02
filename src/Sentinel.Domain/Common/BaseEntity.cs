a4e80b300f9ad7dfbb855ef33eba481c0b87adc2
tttkristian <kristian.lopez2@upr.edu>
2026-05-02 14:40:04 -0400

    Add project files.

---
+++ src/Sentinel.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sentinel.Domain.Common
+{
+    internal class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
